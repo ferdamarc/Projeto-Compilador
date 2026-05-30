f548592f9225d5dbd13662c99636ee6aa67a3399
Fernando Daniel Marcelino <fernando.dan98@gmail.com>
2026-05-30 13:04:12 -0300

    feat(uart): add UART instructions for sending data and checking TX status

--- src/binario.c
+++ src/binario.c
@@ -44,6 +44,10 @@ unsigned int get_opcode(char* nome, instruction_type_t tipo) {
     /* Draw Pixel Instruction */
     else if (strcmp(nome, "draw_pixel") == 0)   opcode = 0b001111;
 
+    /* UART Instructions (TX) */
+    else if (strcmp(nome, "uart_send") == 0)     opcode = 0b011000;
+    else if (strcmp(nome, "uart_tx_ready") == 0) opcode = 0b011011;
+
     /* System Control */
     else if (strcmp(nome, "halt") == 0)  opcode = 0b111111;
     
