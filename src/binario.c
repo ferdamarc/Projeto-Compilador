6f2ef9eeef4dbfe9ba4aaebafab2d6b67cdaff25
Fernando Daniel Marcelino <fernando.dan98@gmail.com>
2026-05-31 11:43:38 -0300

    feat(binario): add UART receive instructions to opcode mapping

--- src/binario.c
+++ src/binario.c
@@ -48,6 +48,10 @@ unsigned int get_opcode(char* nome, instruction_type_t tipo) {
     else if (strcmp(nome, "uart_send") == 0)     opcode = 0b011000;
     else if (strcmp(nome, "uart_tx_ready") == 0) opcode = 0b011011;
 
+    /* UART Instructions (RX) */
+    else if (strcmp(nome, "uart_receive") == 0)      opcode = 0b011001;
+    else if (strcmp(nome, "uart_rx_available") == 0) opcode = 0b011010;
+
     /* System Control */
     else if (strcmp(nome, "halt") == 0)  opcode = 0b111111;
     
