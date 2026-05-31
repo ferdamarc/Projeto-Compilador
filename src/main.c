1e0164618b676c05208d103f47192fe24d39c382
Fernando Daniel Marcelino <fernando.dan98@gmail.com>
2026-05-31 11:43:18 -0300

    feat(uart): add UART receive functions to symbol table

--- src/main.c
+++ src/main.c
@@ -150,6 +150,8 @@ int main(int argc, char *argv[]) {
     insert_symbol(hash_table, DECL_FUNC, TYPE_VOID, "draw_pixel", "global", 0);
     insert_symbol(hash_table, DECL_FUNC, TYPE_VOID, "uart_send", "global", 0);
     insert_symbol(hash_table, DECL_FUNC, TYPE_INT,  "uart_tx_ready", "global", 0);
+    insert_symbol(hash_table, DECL_FUNC, TYPE_INT,  "uart_receive", "global", 0);
+    insert_symbol(hash_table, DECL_FUNC, TYPE_INT,  "uart_rx_available", "global", 0);
 
     // Análise semântica
     if (syntax_tree != NULL) {
